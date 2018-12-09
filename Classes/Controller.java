cd7cf3e9b240792b2e46196beb4a7cb27bfdead0
shahrozahmad90 <l164138@lhr.nu.edu.pk>
2018-12-09 23:11:53 +0500

    Set Controller as for JPA

--- Classes/Controller.java
+++ Classes/Controller.java
@@ -1,88 +1,75 @@
-package safecity.Classes;
+package Classes;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import safecity.API.SendMail;
+import API.SendMail;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Transient;
 
-// Controller is based on Singleton Design Pattern
+// CONTROLLER class is based on Singleton Design Pattern
+
 public class Controller {
+    @Transient
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("SafeCityModalPU");
+    @Transient
+    private static final EntityManager em = (EntityManager) emf.createEntityManager();
     
-    private static final Controller controller = new Controller();
-    private static Connection DBConnection;
-    private static Statement DBStatement;
+    private Controller(){}
     
-    private Controller(){
+    public static EntityManager getManager(){
+        return em;
+    }
+    
+    // THIS METHOD RETURNS THE OWNER REGISTERED TO ANY VEHICLE IF ANY
+    public static Person vehicleRegistrationLookup(String registrationNumber){
         try{
-            // ENTER DATABASE URL HERE
-            DBConnection = DriverManager.getConnection("DATABASE URL HERE");
-            DBStatement = DBConnection.createStatement();
-        } catch(SQLException e){
-            e.printStackTrace();
+            Vehicle v = (Vehicle) em.createNamedQuery("Vehicle.findByRegistrationnumber")
+                        .setParameter("registrationnumber", registrationNumber)
+                        .getSingleResult();
+            
+            Person p = (Person) em.createNamedQuery("Person.findByCnic")
+                        .setParameter("cnic", v.getCnic())
+                        .getSingleResult();
+            return p;
+        }
+        catch(Exception e){
+            throw e;
         }
     }
     
-    public static Controller getInstance(){
-        return controller;
-    }
-    
-    public void sendChallan(Challan challan, Person person){
-        
-        // SAVING CHALLAN IN DATABASE BEFORE MAILING
-        saveChallan(challan);
-        
-        // PLEASE ENTER GMAIL DETAILS HERE BEFORE SENDING MAIL
-        String user = "GMAIL ADDRESS";
-        String pass = "PASSWORD";
-        
-        // SENDING CHALLAN TO VEHICLE OWNER
-        String message = "You've been charged with " + challan.getChallanAmount()
-                         + "against " + challan.getRegistrationNumber() + " (Registration Number)"
-                         + "for :\n" + challan.getChallanReason() + "\n"
-                         + "Challan is issued on " + challan.getIssueDate() + " and due on "
-                         + challan.getDueDate() + ". Pay it asap.";
-        SendMail.send(person.getEmail(), "CHALLAN againt " + challan.getRegistrationNumber(), message, user, pass);
-    }
-    
-    public void saveChallan(Challan challan){
-        String query = "Insert into Challans values ( " + challan.getChallanAmount() + ", '" + challan.getChallanReason() +
-                                                      "', '" + challan.getIssueDate() + "', '" + challan.getDueDate() + 
-                                                      "', '" + challan.getRegistrationNumber() + "')";
-        
-        try{
-            DBStatement.executeUpdate(query);
-        } catch(SQLException e){
-            e.printStackTrace();
+    // THIS METOHD SAVES CHALLAN IN THE DATABASE
+    private static void saveChallan(Challan challan){
+        try {
+            em.getTransaction().begin();
+            em.persist(challan);
+            em.getTransaction().commit();
+        }
+        catch(Exception e){
+            throw e;
         }
     }
     
-    public Person vehicleRegistrationLookup(String registrationNumber){
-        String CNICQuery = "Select OwnerCNIC from Vehicles where RegistrationNumber = '" + registrationNumber + "'";
-        try{
-            // LOOKING CNIC LINKED WITH REGISTRATION NUMBER
-            ResultSet result = DBStatement.executeQuery(CNICQuery);
-            if(!result.next()){
-                // LOOKING PERSON WITH ABOVE CNIC
-                String ownerCNIC = result.getString("OwnerCNIC");
-                result = DBStatement.executeQuery("Select * from Persons where CNIC = '" + ownerCNIC + "'");
-                if(!result.next()){
-                    String name = result.getString("Name");
-                    String email = result.getString("Email");
-                    String phoneNumber = result.getString("PhoneNumber");
-                    return new Person(name, ownerCNIC, email, phoneNumber);
-                }
-                else{
-                    return null;
-                }
-            }
-            else{
-                return null;
-            }
-        } catch(SQLException e){
-            e.printStackTrace();
+    // THIS METHOD SAVES AND MAILS CHALLAN
+    public static boolean sendChallan(Challan challan, Person person) {
+        try {
+            
+            saveChallan(challan);
+            
+            // PLEASE ENTER GMAIL DETAILS HERE BEFORE SENDING MAIL
+            String user = "GMAIL ADDRESS";
+            String pass = "PASSWORD";
+
+            // SENDING CHALLAN TO VEHICLE OWNER
+            String message = "You've been charged with " + challan.getAmount()
+                    + "against " + challan.getRegistrationnumber() + " (Registration Number)"
+                    + "for :\n" + challan.getReason() + "\n"
+                    + "Challan is issued on " + challan.getIssuedate() + " and due on "
+                    + challan.getDuedate() + ". Pay it asap.";
+            SendMail.send(person.getEmail(), "CHALLAN againt " + challan.getRegistrationnumber(), message, user, pass);
+            return true;
         }
-        return null;
+        catch(Exception e){
+            throw e;
+        }
     }
 }
